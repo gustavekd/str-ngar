888e614783112552e7e262cc9e7f5c965ea59e51
gustavekd <gustav.ekelund99@gmail.com>
2016-10-07 12:26:35 +0200

    övning

--- strängar/Program.cs
+++ strängar/Program.cs
@@ -9,7 +9,33 @@ namespace strängar
     class Program
     {
         static void Main(string[] args)
+        //övning1
         {
+            Console.WriteLine("\u0056\u00E4\u0157\u006D\u0064\u00F6\0\u0047\u0079\u006D\u006E\u0061\u0073\u0069\u0075\u006D");
+
+
+            //övning2,3a,b
+
+            string myString = "Hello World";
+            Console.WriteLine("\"Hello World!\"");
+            Console.WriteLine(myString.ToUpper());
+            Console.WriteLine(myString.ToLower());
+
+            //övning 4        
+            Console.WriteLine("Hello World är " + myString.Length + " tecken lång");
+
+            //övning 5 
+            Console.Write("skriv en text: ");
+            string bakofram = Console.ReadLine();
+
+            for (int i = bakofram.Length - 1; i >= 0; i--)
+            {
+                Console.Write(bakofram[i]);
+            }
+
+                      
+
         }
+
     }
 }
